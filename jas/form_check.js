e0c99da854906cb5988c9db5a84ed9559a32f968
Gromoboy <amejuev@gmail.com>
2018-11-08 01:51:11 +0300

    дз 5 зд 3 Валидация формы

---
+++ jas/form_check.js
@@ -0,0 +1,46 @@
+// Урок 5, задание 3:
+//
+// Создать форму в html со следующими полями:
+//   * Имя - текстовое поле
+// * Телефон - текстовое поле
+// * Пароль - поле типа password
+// * Повтор пароля - поле типа password
+// * Кнопка отправить форму
+//
+// Необходимо реализовать валидацию этой формы по следующим правилам:
+//
+//   * Имя - должно содержать как минимум 1 символ, не более 50 символов.
+// * Телефон - должно содержать 11 цифр, не больше, не меньше.
+// * Пароль - минимум 5 символов, максимум 50.
+// * Повтор пароля - значение должно совпадать с полем пароль.
+// * Кнопка отправить форму - при нажатии на кнопку форма должна провериться, при прохождении проверки, форма
+// отправляется, если проверка не была пройдена, то:
+// - Каждое поле, которое не прошло проверку должно выделяться красным цветом.
+// - Под каждым полем, что не прошло проверку, должна писаться подсказка по какой причине проверка провалилась.
+
+  function checkForm(event) {
+  "use strict";
+
+  // debugger;
+  if (!event.target.classList.contains("btn-primary")) return;
+  const userName = document.getElementsByName("name").item(0);
+  const userTel  = document.getElementsByName("phone").item(0);
+  const userPass = document.getElementsByName("password").item(0);
+  const userRepP = document.getElementsByName("password_repeat").item(0);
+  if (userTel.value.length !== 11) {
+    event.preventDefault();
+    userTel.classList.add("is-invalid");
+  }
+  if (userName.value.length < 1 || userName.value.length > 50) {
+    event.preventDefault();
+    userName.classList.add("is-invalid");
+  }
+  if (userPass.value.length < 5 || userPass.value.length > 50) {
+    event.preventDefault();
+    userPass.classList.add("is-invalid");
+  }
+  if (userRepP.value !== userPass.value) {
+    event.preventDefault();
+    userRepP.classList.add("is-invalid");
+  }
+}
